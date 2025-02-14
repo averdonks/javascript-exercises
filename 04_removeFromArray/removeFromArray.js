446854871e4b2985eda9a520ac12c4c8757d0e1a
averdonks <62974106+averdonks@users.noreply.github.com>
2025-02-14 17:02:04 -0500

    Submit solution for removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, value1, value2, value3, value4) {
+    for (let i = 0; i <= array.length - 1; i++) {
+        if (array[i] === value1 || array[i] === value2 ||
+            array[i] === value3 || array[i] === value4) {
+            array.splice(i, 1);
+            i = -1;
+        } 
+    }
+    return array;
 };
 
 // Do not edit below this line
