5e14ab259a2dfb0b72537dd15c494def17ddddcb
averdonks <62974106+averdonks@users.noreply.github.com>
2025-02-15 12:33:51 -0500

    Submit solution for tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFahrenheit) {
+  let degreesCelsius = ((degreesFahrenheit - 32) * 5/9).toFixed(1);
+  return Number(degreesCelsius); 
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  let degreesFahrenheit = ((degreesCelsius * 9/5) + 32).toFixed(1);
+  return Number(degreesFahrenheit);
 };
 
 // Do not edit below this line
