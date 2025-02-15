c8c1c032c910f68c37fc1063df9e4dc38499cf76
averdonks <62974106+averdonks@users.noreply.github.com>
2025-02-15 11:56:26 -0500

    Edit solution for leapYears

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,9 +1,6 @@
 const leapYears = function(year) {
-    if (year % 4 === 0) {
-        if (year % 100 === 0 && year % 400 === 0) return true;
-        else if (year % 100 != 0) return true;
-        else return false;
-    } else return false;
+    if (year % 4 === 0 && (year % 100 != 0 || year % 400 === 0)) return true;
+    else return false;
 };
 
 // Do not edit below this line
