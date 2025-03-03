8f350207a53f5882f1fd048d3482672090f54a32
averdonks <62974106+averdonks@users.noreply.github.com>
2025-03-03 12:19:37 -0500

    Submit solution for palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,29 @@
-const palindromes = function () {
+const palindromes = function (word) {
+    let formattedWord = word.split("")
 
-};
+    for (let i = 0; i < formattedWord.length; i++) {
+        formattedWord[i] = formattedWord[i].toLowerCase();
+        if (formattedWord[i] === "." ||
+            formattedWord[i] === "," ||
+            formattedWord[i] === "!" ||
+            formattedWord[i] === "?" ||
+            formattedWord[i] === " "
+        ) {
+            formattedWord.splice(i, 1);
+            i--;
+        }
+    }
+
+    let reverse = formattedWord.slice();
+    reverse = reverse.reverse().join("");
+    console.log(reverse);
+
+    formattedWord = formattedWord.join("");
+    
+    console.log(formattedWord);
+    console.log(reverse);
+    return reverse === formattedWord;
+}
 
 // Do not edit below this line
 module.exports = palindromes;
