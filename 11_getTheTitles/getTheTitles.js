f46218ed0782eaad9d680949c41053b2132fde7a
averdonks <62974106+averdonks@users.noreply.github.com>
2025-03-03 15:38:05 -0500

    Submit solution for getTheTitles

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,19 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
 
+const titles = books.map((book) => {
+    return book.title;
+});
+
+return titles;
 };
 
 // Do not edit below this line
+
 module.exports = getTheTitles;
+
+// const getTheTitles = function(books) {
+
+    // const titles = books.map(book => book.title);
+    
+    // return titles;
+    // };
