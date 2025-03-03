398d786280f31375f89934d1487079b3f45a8fea
averdonks <62974106+averdonks@users.noreply.github.com>
2025-03-03 14:48:46 -0500

    Submit solution for fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,27 @@
-const fibonacci = function() {
+const fibonacci = function(max) {
+    let oneSumAgo = 0;
+    let twoSumsAgo = 0;
+    let fibonacciNum = 0;
 
+    if (Number(max) < 0) {
+        fibonacciNum = "OOPS";
+    }
+
+    for (let i = 0; i <= Number(max); i++) {
+        if (Number(max) === 0) {
+            break;
+        } else if (i === 1) {
+            oneSumAgo = 1;
+            twoSumsAgo = 0;
+            fibonacciNum = oneSumAgo + twoSumsAgo;
+        } else {
+            fibonacciNum = oneSumAgo + twoSumsAgo;
+            twoSumsAgo = oneSumAgo;
+            oneSumAgo = fibonacciNum;
+        }
+    }
+
+    return fibonacciNum;
 };
 
 // Do not edit below this line
