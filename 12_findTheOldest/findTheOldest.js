ddeb80e98ea0cb5b802852b6dee5a8b995c959ab
averdonks <62974106+averdonks@users.noreply.github.com>
2025-03-04 10:24:35 -0500

    Submit solution for findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(arr) {
+    let age = 0;
+    let greatestAge = 0;
+    let oldest = {};
 
+    arr.map((person) => {
+        if (!person.hasOwnProperty("yearOfDeath")) {
+            person.yearOfDeath = new Date().getFullYear();
+        }
+        age = person.yearOfDeath - person.yearOfBirth;
+        if (age > greatestAge) {
+            greatestAge = age;
+            oldest = person;
+        }
+    });
+
+    return oldest;
 };
 
 // Do not edit below this line
